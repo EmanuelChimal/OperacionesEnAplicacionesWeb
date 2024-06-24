80ac8bfc85844ebca0c163aaa856e989022248f0
Emanuel Chimal <armendarizchimalemanuel@gmail.com>
2024-06-23 18:29:50 -0600

    Update loc

--- OperacionesAplicacionesWEB/src/java/Componentes/ActualizarLocalidades.java
+++ OperacionesAplicacionesWEB/src/java/Componentes/ActualizarLocalidades.java
@@ -6,26 +6,11 @@ package Componentes;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Alan DoubleEight4
- */
+
 public class ActualizarLocalidades extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
